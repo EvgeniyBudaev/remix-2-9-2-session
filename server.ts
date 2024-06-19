00b2472a4eb289f3ad77a35f9c97b3338628b954
Evgeniy Budaev <budaev.e@gmail.com>
2024-06-19 18:50:08 +0300

    init

---
+++ server.ts
@@ -0,0 +1,44 @@
+import { createRequestHandler } from '@remix-run/express';
+import { createCookie, createFileSessionStorage } from '@remix-run/node';
+import express from 'express';
+
+const viteDevServer =
+  process.env?.NODE_ENV === "production"
+    ? null
+    : await import("vite").then((vite) =>
+      vite.createServer({
+        server: { middlewareMode: true },
+      })
+    );
+
+const app = express();
+
+app.use(
+  viteDevServer
+    ? viteDevServer.middlewares
+    : express.static("build/client")
+);
+
+const build = viteDevServer
+  ? () =>
+    viteDevServer.ssrLoadModule(
+      "virtual:remix/server-build"
+    )
+  : await import("./build/server/index.js");
+
+const sessionCookie = createCookie("__session", {
+  secrets: ["r3m1xr0ck5"],
+  sameSite: true,
+});
+
+const session =
+  createFileSessionStorage({
+    dir: "sessions",
+    cookie: sessionCookie,
+  });
+
+app.all("*", createRequestHandler({ build }));
+
+app.listen(3000, () => {
+  console.log("App listening on http://localhost:3000");
+});
